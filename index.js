57b26441b31def18c06c1b3118fd8407c86fd5bf
José Miguel Visa <josemiguel_18113@outlook.es>
2022-06-22 22:21:14 -0300

    mi primer commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let mesesitos = [
+    { mes: "enero", ganancia: 870 },
+    { mes: "febrero", ganancia: 1000 },
+    { mes: "marzo", ganancia: 1020 },
+    { mes: "abril", ganancia: 2300 },
+    { mes: "mayo", ganancia: -10 }
+];
+
+function agregar(unaLista, elemento) {
+    unaLista.push(elemento);
+}
+
+function getMeses(array) {
+    let meses = [];
+    for (let element of array) {
+        agregar(meses, element.mes);
+    }
+    return meses;
+}
+
+function afortunados(array) {
+    let afortunados = []
+    for (let element of array) {
+        if (element.ganancia > 1000) {
+            agregar(afortunados, element)
+        }
+    }
+    return afortunados;
+}
+
+function mesesAfortunados(array) {
+    return getMeses(afortunados(array));
+}
+
+getMeses(mesesitos);
+
+afortunados(mesesitos);
+
+mesesAfortunados(mesesitos);
